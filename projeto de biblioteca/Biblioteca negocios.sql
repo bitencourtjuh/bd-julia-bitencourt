bf1bd9161aebcf0d3d17b7da4b2862f3bec5ba41
Julia Bitencourt <minhajuliabitencourt500@gmail.com>
2025-04-04 01:22:29 -0300

    codando em banco de dados

---
+++ projeto de biblioteca/Biblioteca negocios.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE negocios;
+CREATE TABLE regra_de_negocios( 
+Id INT PRIMARY KEY ,
+regra VARCHAR (100)
+);
+INSERT INTO regra_de_negocios VALUES ("RN01", "Limite de 3 livros por usuário ");
+INSERT INTO regra_de_negocios VALUES ("RN02", "Prazo de devolução: 14 dias ");
+INSERT INTO regra_de_negocios VALUES ("RN03", "Multa de R$ 1,00 por dia de atraso ");
+INSERT INTO regra_de_negocios VALUES ("RN04", "Permitir uma renovação por livro ");
+INSERT INTO regra_de_negocios VALUES ("RN05", "Cadastro obrigatório de novos livros ");
+
